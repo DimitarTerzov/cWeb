e3afb21392508767e0c8e0fb3387a7ff81587b39
Dimitar Terzov <dimiterat@gmail.com>
2020-02-13 15:48:45 +0200

    Start fixing.

--- src/tests/test_command_15.py
+++ src/tests/test_command_15.py
@@ -8,10 +8,12 @@ CONTENT =[
     "word ~ word\n",
     "word~~ word\n",
     "word~ word\n",
+    "hello world~word\n",
     "word ~word\n",
     " ~word\n",
     "hello word~ word\n",
-    "hello word~ ."
+    "hello word~ .\n",
+    "hello world~."
 ]
 
 
@@ -26,12 +28,9 @@ def file_with_tildes(tmpdir):
 
 def test_command15(file_with_tildes):
     found = command15(file_with_tildes)
-    with open(file_with_tildes) as file_:
-        for line in file_.readlines():
-            print(line.rstrip())
-    for value in found.values():
-        assert "word~ word" not in value
-        assert "word ~word" not in value
-        assert "hello word~ ." not in value
-        assert "hello word~ word" not in value
-        assert " ~word" not in value
+    print(found)
+
+    assert "word~word" in found[0]
+    assert "world~word" in found[4]
+    assert "world~" in found[9]
+    assert 0
