e3671b8cf93b5228cad21376e96db9b0f7844a9f
Dimitar Terzov <dimiterat@gmail.com>
2020-02-25 20:19:58 +0200

    Use OrderedDict to maintain order for errors.

--- src/problem_13/validator_13.py
+++ src/problem_13/validator_13.py
@@ -3,15 +3,21 @@ import re
 
 #Speaker validator
 def command13(filepath):
+    from collections import OrderedDict
 
     regex = re.compile('<Turn (?:speaker="(spk[0-9]+)")?(?:.*)startTime="([0-9.]+)"(?:.*) (?:speaker="(spk[0-9]+)")?')
 
-    found = {}
+    found = OrderedDict()
 
     prev_spk='none'
     sync = False
     sync_count = 0
     end_time = 0
+    turns_out_of_order = []
+    syncs_out_of_order = []
+    empty_segments = []
+    empty_turns = []
+    sequential_turns = []
 
     with open(filepath,'r') as f:
         ln = -1
@@ -29,7 +35,7 @@ def command13(filepath):
 
                 # Catch turns out of order
                 if start_value != end_time:
-                    found[ln] = [13, "Turn out of chronological order", start_time]
+                    turns_out_of_order.append((ln, [13, "Turn out of chronological order", start_time]))
 
                 end_time = re.search(r'endTime="(?P<value>\d+\.?\d*)"', line)
                 end_time = float(end_time.group('value'))
@@ -45,23 +51,23 @@ def command13(filepath):
                 if sync_count == 1:
                     # compare sync_time with start_value
                     if sync_time_value != start_value:
-                        found[ln] = [13, "Segments out of chronological order", new_sync_time]
+                        syncs_out_of_order.append((ln, [13, "Segments out of chronological order", new_sync_time]))
 
                 elif sync_count > 1:
                     # compare new sync_time with old sync_time
                     old_sync_value = re.search(r'(\d+\.?\d*)', sync_time)
                     if sync_time_value <= float(old_sync_value.group()):
-                        found[ln] = [13, "Segments out of chronological order", new_sync_time]
+                        syncs_out_of_order.append((ln, [13, "Segments out of chronological order", new_sync_time]))
 
                 sync_time = new_sync_time
 
             elif "</Turn>" == line and sync and sync_count == 1:
-                found[ln] = [13, "Empty turns are not allowed", start_time]
+                empty_turns.append((ln, [13, "Empty turns are not allowed", start_time]))
                 sync = False
                 sync_count = 0
 
             elif 'Sync' in line and sync:
-                found[ln] = [13, "Empty segments are not allowed", sync_time]
+                empty_segments.append((ln, [13, "Empty segments are not allowed", sync_time]))
                 sync_count += 1
                 new_sync = re.search(r'(?P<content>Sync time="(?P<value>\d+\.?\d*)")', line)
                 new_sync_time = new_sync.group('content')
@@ -70,7 +76,7 @@ def command13(filepath):
                 # Compare new sync_time with old sync_time
                 old_sync_value = re.search(r'(\d+\.?\d*)', sync_time)
                 if sync_time_value <= float(old_sync_value.group()):
-                    found[ln] = [13, "Segments out of chronological order", new_sync_time]
+                    syncs_out_of_order.append((ln, [13, "Segments out of chronological order", new_sync_time]))
 
                 sync_time = new_sync_time
 
@@ -78,7 +84,7 @@ def command13(filepath):
                 sync = False
 
             elif "</Turn>" == line and sync and sync_count > 1:
-                found[ln] = [13, "Empty segments are not allowed", sync_time]
+                empty_segments.append((ln, [13, "Empty segments are not allowed", sync_time]))
                 sync = False
                 sync_count = 0
 
@@ -95,10 +101,38 @@ def command13(filepath):
                     speaker = m[0] if m[0] != '' else m[2]
 
                     if speaker == prev_spk:
-                        found[ln] = [13, 'Sequential turns by the same speaker', speaker + " at " + m[1]]
+                        sequential_turns.append((ln, [13, 'Sequential turns by the same speaker', speaker + " at " + m[1]]))
 
                 #save speaker
                 prev_spk = speaker
+
+    for item in syncs_out_of_order:
+        key, value = item
+        found[key] = value
+
+    for item in turns_out_of_order:
+        key, value = item
+        if key in found:
+            continue
+        found[key] = value
+
+    for item in empty_segments:
+        key, value = item
+        if key in found:
+            continue
+        found[key] = value
+
+    for item in empty_turns:
+        key, value = item
+        if key in found:
+            continue
+        found[key] = value
+
+    for item in sequential_turns:
+        key, value = item
+        if key in found:
+            continue
+        found[key] = value
 
     return found
 
@@ -106,7 +140,5 @@ def command13(filepath):
 if __name__ == "__main__":
 
     found = command13('../files/test_13.trs')
-    keys = found.keys()
-    keys = sorted(keys)
-    for key in keys:
-        print key, '; ', found[key]
+    for key, value in found.iteritems():
+        print key, '; ', value
