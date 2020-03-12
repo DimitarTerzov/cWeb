586e735278533c73e116c521939cb5fd45efda4b
Dimitar Terzov <dimiterat@gmail.com>
2020-03-12 10:49:09 +0200

    Remove redundant functionalities.

--- src/command_14/command_14.py
+++ src/command_14/command_14.py
@@ -1,21 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import io
 import re
-
-from app.cWeb import WWwhitespace
-
 
 #Segment length validator
 def command14(filepath):
 
-    regex = re.compile('<Sync time="\s*([0-9\.]+)\s*"/>')
-    regez = re.compile("<Sync time=\"" + WWwhitespace +"+[0-9\.]+\"/>|<Sync time=\"[0-9\.]+"+ WWwhitespace+"\"/>|<Sync time=\""+ WWwhitespace +"+[0-9\.]+"+ WWwhitespace+"\"/>")
+    regex = re.compile(ur'<Sync\s*time\s*=\s*"\s*([0-9\.]+)\s*"\s*/>')
 
     found = {}
-    cur_time = 0.0
+    cur_time = 0
 
-    with open(filepath,'r') as f:
-        ln = -1
+    with io.open(filepath,'r') as f:
+        ln = 1
         for line in f:
-            ln = ln + 1
             line = line.rstrip("\r\n")
             for m in re.findall(regex, line):
 
@@ -23,21 +22,17 @@ def command14(filepath):
                 seg_len = seg_time - cur_time
 
                 if seg_len > 15.0:
-                    found[ln] = [14, 'Segment exceeds limit', 'Sync time="' + str(cur_time) + '" length: ' + str(seg_len) + ' seconds']
+                    found[ln] = [14, 'Segment exceeds limit', 'Sync time="{}"; length: {} seconds'.format(seg_time, seg_len)]
 
                 #update current time
                 cur_time = seg_time
-            for m in re.findall(regez, line):
 
-                found[ln] = [14, 'Unexpected white space in sync time tag', line]
-
+            ln += 1
 
     return found
 
 
 if __name__ == '__main__':
-    #print(WWwhitespace)
-    found = command14('RNZ_Insight_002.trs')
-    print(found)
-
-
+    found = command14('../files/RNZ_Insight_002.trs')
+    for key in sorted(found.keys()):
+        print(key, found[key])
