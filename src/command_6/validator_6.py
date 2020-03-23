131261436ff4892a5bbe54c7e67988edaeda8ecd
Dimitar Terzov <dimiterat@gmail.com>
2020-03-23 07:48:26 +0200

    Improvement in 6.

--- src/command_6/validator_6.py
+++ src/command_6/validator_6.py
@@ -10,7 +10,7 @@ def command6(filepath):
 
     found = {}
 
-    with open(filepath) as f:
+    with io.open(filepath, 'r', encoding='utf') as f:
         ln = -1
         for line in f:
             ln = ln + 1
@@ -21,9 +21,21 @@ def command6(filepath):
                 #we skip everythin between <>
                 continue
 
-            for word in line.split():
-                if re.match('\S*\d+\S*', word) and not (word.startswith('<') and word.endswith('>')):
-                    found[ln] = [6, 'Numerals not allowed', word]
+            words = re.findall(ur'\b\w+\b', line, re.UNICODE)
+            for word in words:
+
+                if re.match(ur'\S*\d+\S*', word, re.UNICODE) and not (word.startswith('<') and word.endswith('>')):
+
+                    index = words.index(word)
+                    if index > 0:
+                        content = u'{} {}'.format(words[index - 1], word).encode('utf')
+                    elif index == 0 and len(words) > 1:
+                        content = u'{} {}'.format(word, words[1]).encode('utf')
+                    else:
+                        content = u'{}'.format(word).encode('utf')
+
+                    found[ln] = [6, 'Numerals not allowed', content]
+
     return found
 
 
@@ -32,3 +44,4 @@ if __name__ == '__main__':
 
     for key in sorted(found.keys()):
         print(key, found[key])
+        print(found[key][2])
