d5e2e61ac8667467a7400aa777bb1a947a1f1e2c
Dimitar Terzov <dimiterat@gmail.com>
2020-03-31 14:38:05 +0300

    Implementation of command 0.

--- src/command_0/validator_0.py
+++ src/command_0/validator_0.py
@@ -1,2 +1,46 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import re
+import io
+
+
+LANGUAGE_CODES = {
+    u'bul': u'Bulgarian',
+    u'eng': u'English',
+    u'ell': u'Greek'
+}
+
+
+# Transcribers validator
 def command0(filepath):
-    pass
+    transcriber_pattern = re.compile(ur'\s*<\s*Trans\s*scribe\s*=.*?(?P<scriber>\w+?\-\d\d\d)\s*"', re.UNICODE)
+
+    found = {}
+    with io.open(filepath, 'r', encoding='utf') as f:
+        ln = 0
+        for line in f:
+            line = line.rstrip('\r\n')
+
+            if re.search(ur'\s*<\s*Trans\s*scribe\s*=', line, re.UNICODE) is not None:
+
+                match = re.search(transcriber_pattern, line)
+                if match is not None:
+
+                    language_code = match.group('scriber')[:-4]
+                    if language_code in LANGUAGE_CODES:
+                        found['transcriber_id'] = match.group('scriber')
+                    else:
+                        found[ln] = [0, 'Incorrect Transcriber ID', line.encode('utf')]
+
+                else:
+                    found[ln] = [0, 'Incorrect Transcriber ID', line.encode('utf')]
+
+                break
+
+            ln += 1
+
+    return found
+
+
+if __name__ == '__main__':
+    found = command0('')
--- src/tests/test_command_0.py
+++ src/tests/test_command_0.py
@@ -1,7 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import os
+
 import pytest
 
 from command_0.validator_0 import command0
+from utils import temporary_file, LANGUAGE_CODES
 
 
-def test_command_0():
-    pass
+CONTENT = [
+    [u'<Trans scribe="ell-001" audio_filename="EPT_Political_news_118" version="9" version_date="200227">\n'],      # 0
+    [u'<Trans scribe="bul-011" audio_filename="EPT_Political_news_118" version="9" version_date="200227">\n'],     # 1
+    [u'<Trans scribe="ellis-101" audio_filename="EPT_Political_news_118" version="9" version_date="200227">\n'],    # 2
+    [u'<Trans scribe="ell-a01" audio_filename="EPT_Political_news_118" version="9" version_date="200227">\n'],       # 3
+    [u'<Trans scribe="ell-0010" audio_filename="EPT_Political_news_118" version="9" version_date="200227">\n'],     # 4
+]
+
+
+def test_command_0(tmpdir):
+    for row in range(5):
+        file_ = temporary_file(tmpdir, CONTENT[row])
+        found = command0(file_)
+
+        for key, value in found.items():
+            print(key, value)
+
+        if row in [2, 3, 4]:
+            assert 0 in found
+        else:
+            assert 'transcriber_id' in found
+
+    assert 0
