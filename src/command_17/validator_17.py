843d0deb4bf34b8cc1f0a45a70256b8368a8dc3f
Dimitar Terzov <dimiterat@gmail.com>
2020-04-14 18:28:20 +0300

    Fix synq times mismatch.

--- src/command_17/validator_17.py
+++ src/command_17/validator_17.py
@@ -1,30 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import re
+import io
+
+
 #Short turns
 def command17(filepath):
 
-    regex = re.compile('<Sync time="\s*([0-9\.]+)\s*"/>')
-    regez = re.compile("<Sync time=\"" + WWwhitespace +"+[0-9\.]+\"/>|<Sync time=\"[0-9\.]+"+ WWwhitespace+"\"/>|<Sync time=\""+ WWwhitespace +"+[0-9\.]+"+ WWwhitespace+"\"/>")
+    regex = re.compile(ur'<Sync time="\s*([0-9\.]+)\s*"/>', re.UNICODE)
 
     found = {}
-    cur_time = 0.0
+    cur_time = None
 
-    with open(filepath,'r') as f:
-        ln = -1
+    with io.open(filepath, 'r', encoding='utf') as f:
+        ln = 0
         for line in f:
             ln = ln + 1
             line = line.rstrip("\r\n")
             for m in re.findall(regex, line):
 
                 seg_time = float(m)
-                seg_len = seg_time - cur_time
+                if cur_time is not None:
+                    seg_len = seg_time - cur_time
 
-                if seg_len < 3.0:
-                    found[ln] = [17, 'Segment is less than 3 seconds, possible use of [overlap] or combine with other segment', 'Sync time="' + str(cur_time) + '" length: ' + str(seg_len) + ' seconds']
+                    if seg_len < 3.0:
+                        found[last_seq_row + 1] = [17, 'Segment is less than 3 seconds, possible use of [overlap] or combine with other segment', 'Sync time="' + str(cur_time) + '" length: ' + str(seg_len) + ' seconds']
 
                 #update current time
                 cur_time = seg_time
-            for m in re.findall(regez, line):
-
-                found[ln] = [17, 'Unexpected white space in sync time tag', line]
-
+                last_seq_row = ln
 
     return found
+
+
+if __name__ == '__main__':
+    found = command17('../files/channel0_334.trs')
+    for key in sorted(found.keys()):
+        print(key, found[key])
+        
