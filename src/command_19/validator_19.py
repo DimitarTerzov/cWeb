6233e1ec0ffb87628ab5d9c9fdcab528cc006d6d
Dimitar Terzov <dimiterat@gmail.com>
2020-04-16 11:31:14 +0300

    Catch choppy segments with variable number of chopped words.

--- src/command_19/validator_19.py
+++ src/command_19/validator_19.py
@@ -6,22 +6,29 @@ import io
 
 # Choppy segments
 def command19(filepath):
-    partial_line_end_marks = u":,\-_!—.?;"
+    # Segments shorter than that amount of time in seconds
+    # are potentially Chopped segments. You can control it
+    # by changing this value.
+    time_amount_left = 12    # Y
+    # Segments that contain that number of words at the beginning
+    # or at end are potentially Chopped segments. You can control
+    # this by changing the value of variable.
+    number_of_words = 2     # X
+
+    partial_line_end_marks = u":,\-_!—.?;\]"
     line_end_marks = u'!.?'
     turn_end = re.compile(ur'<\s*/\s*Turn\s*>', re.UNICODE)
-    time_amount_left = 12    # Y
-    number_of_words = 1    # X
 
     found = {}
     with io.open(filepath, 'r', encoding='utf') as f:
 
-        ln = 0
+        ln = 1
         sync_time = None
         segment_lenght = None
         in_turn = False
         check_for_choppy = False
         for line in f:
-            line = line.rstrip("\r\n")
+            line = line.rstrip(" \r\n")
 
             if not line:
                 ln += 1
@@ -31,44 +38,68 @@ def command19(filepath):
                 sync_time = None
                 segment_lenght = None
                 chopped_at_end = False
+                chopped_line_end = None
+                check_for_choppy = False
                 in_turn = True
 
-            elif in_turn and re.search(ur'<\s*Sync\s*time', line, re.UNICODE) is not None:
-                new_sync_time = re.search(ur'<\s*Sync\s*time\s*=\s*"\s*(?P<value>[\d.]+?)\s*"', line, re.UNICODE).group('value')
-                new_sync_time = float(new_sync_time)
-                if sync_time is not None:
-                    segment_lenght = new_sync_time - sync_time
-                sync_time = new_sync_time
+            elif in_turn:
 
-            elif in_turn and re.search(turn_end, line) is None:
+                if re.search(ur'<\s*Sync\s*time', line, re.UNICODE) is not None:
 
-                if check_for_choppy and segment_lenght <= time_amount_left:
-                    index = number_of_words - 1
-                    chopped = line.split()[index]
-                    if (
-                        re.search(ur'[{}]$'.format(line_end_marks), chopped, re.UNICODE) is not None and
-                        chopped[0].islower()
-                    ):
-                        found[ln] = [19, "Choppy segment", line.encode('utf')]
-                
-                if chopped_at_end and segment_lenght <= time_amount_left:
-                    found[ln-2] = [19, "Choppy segment", line.encode('utf')]
+                    new_sync_time = re.search(ur'<\s*Sync\s*time\s*=\s*"\s*(?P<value>[\d.]+?)\s*"', line, re.UNICODE).group('value')
+                    new_sync_time = float(new_sync_time)
+                    if sync_time is not None:
+                        segment_lenght = new_sync_time - sync_time
 
-                if (
-                    re.search(ur'[{}]$'.format(partial_line_end_marks), line, re.UNICODE) is None
-                ):
-                    chopped = line.split()[-(number_of_words + 1)]
-                    if re.search(ur'[{}]$'.format(line_end_marks), chopped, re.UNICODE) is not None:
-                        chopped_at_end = True
-                       
-                    check_for_choppy = True
-                else:
-                    check_for_choppy = False
+                    sync_time = new_sync_time
+                    in_sync = True
+
+                elif in_sync:
+
+                    if check_for_choppy and segment_lenght <= time_amount_left:
+
+                        for index in xrange(number_of_words):
+                            try:
+                                chopped = line.split()[index]
+                            except IndexError:
+                                pass
+                            else:
+                                if (
+                                    re.search(ur'[{}]$'.format(line_end_marks), chopped, re.UNICODE) is not None and
+                                    chopped[0].islower()
+                                ):
+                                    found[ln] = [19, "Choppy segment", line.encode('utf')]
+                                    break
+
+                    if chopped_at_end and segment_lenght <= time_amount_left and line[0].islower():
+                        found[ln-2] = [19, "Choppy segment", chopped_line_end.encode('utf')]
+
+                    if re.search(ur'[{}]$'.format(partial_line_end_marks), line, re.UNICODE) is None:
+
+                        for index in xrange(-(number_of_words + 1), -1):
+                            try:
+                                chopped = line.split()[index]
+                            except IndexError:
+                                check_for_choppy = False
+                                chopped_at_end = False
+                                chopped_line_end = None
+                            else:
+                                if re.search(ur'[{}]$'.format(line_end_marks), chopped, re.UNICODE) is not None:
+                                    chopped_at_end = True
+                                    chopped_line_end = line
+                                    break
+
+                        check_for_choppy = True
+
+                    else:
+                        check_for_choppy = False
+                        chopped_at_end = False
+                        chopped_line_end = None
+
+                    in_sync = False
 
             elif re.search(turn_end, line) is not None:
-                sync_time = None
                 in_turn = False
-                check_for_choppy = False
 
             ln += 1
 
@@ -76,6 +107,6 @@ def command19(filepath):
 
 
 if __name__ == '__main__':
-    found = command19('../files/CT_Newsevents_40.trs')
+    found = command19('../files/Ykkosaamu_007.trs')
     for key in sorted(found.keys()):
         print(key, found[key])
