a6c81f8f4f7a898878f60dfbbcde8eb22041bc75
Dimitar Terzov <dimiterat@gmail.com>
2020-04-24 16:13:49 +0300

    Improve command 7.

--- src/command_7/validator_7.py
+++ src/command_7/validator_7.py
@@ -9,12 +9,13 @@ def command7(filepath):
     # Allowed punctuation after tag
     punctuation = u"[:',!—_\".?\-;]"
     #default skip tags
-    skip_tags = u"(#uh|#um|#ah|#er|#hm|#อื|#อ่|#เอ่|#เออ)"
+    skip_tags = u"(#uh|#um|#ah|#er|#hm)"
     possible_missing_tag = u"(uh|um|ah|er|hm)"
     filler_re = re.compile(ur'[\W\w]?#\w*\W?', re.UNICODE)
 
     found = {}
     in_section = False
+    tag_exists = False
     with io.open(filepath, 'r', encoding='utf') as f:
         ln = 0
         for line in f:
@@ -27,6 +28,7 @@ def command7(filepath):
                 in_section = False
 
             for match in re.finditer(filler_re, line):
+                tag_exists = True
 
                 target = match.group().strip()
                 # Pass filler tag with tilde.
@@ -41,16 +43,18 @@ def command7(filepath):
                     found[ln] = [7, 'Invalid filler tag', target.encode('utf')]
                     continue
 
-
             for match in re.finditer(ur'\s{0}\W'.format(possible_missing_tag), line, re.UNICODE):
                 if ln not in found and in_section:
                     found[ln] = [7, 'Possible filler tag missing #', match.group().encode('utf')]
+
+    if not tag_exists:
+        found[1] = [7, 'No fillers tags were found. Please refer to the project page to learn about the required use of filler tags.', '']
 
     return found
 
 
 if __name__ == '__main__':
-    found = command7("../files/AsiaWaveNews_08.trs")
+    found = command7("../files/no_tags.trs")
     keys = sorted(found.keys())
     for key in keys:
         print key, found[key], found[key][2]
